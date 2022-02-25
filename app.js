532d1d2720b78e3bb3bb2b4bf271307a440dbf39
Shi_J_C <76093941+Shi-J-C@users.noreply.github.com>
2022-02-25 15:16:32 +0800

    update file size

--- app.js
+++ app.js
@@ -1,51 +1,60 @@
-const express = require('express');
-const mongoose = require('mongoose');
-var bodyParser = require("body-parser");
-var cors = require("cors");
-const PORT = process.env.PORT || 3000;
+const express = require('express')
+const mongoose = require('mongoose')
+var bodyParser = require('body-parser')
+var cors = require('cors')
+const PORT = process.env.PORT || 3000
 
 var corsOptions = {
-    origin: "*"
-};
+  origin: '*',
+}
 
-const userRoutes = require('./routes/user');
-const moduleRoutes = require('./routes/courseModule');
-const postRoutes = require('./routes/post');
-const commentRoutes = require('./routes/comment');
+const userRoutes = require('./routes/user')
+const moduleRoutes = require('./routes/courseModule')
+const postRoutes = require('./routes/post')
+const commentRoutes = require('./routes/comment')
 
+const app = express()
+const user = 'user1'
+const pw = 'blackrabbit1'
+const db = 'Application'
 
-const app = express();
-const user = 'user1';
-const pw = 'blackrabbit1';
-const db = 'Application';
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 
-app.use(cors(corsOptions)); // parse requests of content-type - application/x-www-form-urlencoded
+app.use(cors(corsOptions)) // parse requests of content-type - application/x-www-form-urlencoded
 
-const dbURI = 'mongodb+srv://' + user + ':' + pw + '@scast.nr5lz.mongodb.net/' + db + '?retryWrites=true&w=majority';
+const dbURI =
+  'mongodb+srv://' +
+  user +
+  ':' +
+  pw +
+  '@scast.nr5lz.mongodb.net/' +
+  db +
+  '?retryWrites=true&w=majority'
 //console.log(dbURI);
-mongoose.connect(dbURI)
-    .then((result) => {
-        console.log('connected to db!');
-        //listen for reuqests
-        app.listen(PORT, () => {
-            console.log("server is listening to port " + PORT);
-        });
+mongoose
+  .connect(dbURI)
+  .then((result) => {
+    console.log('connected to db!')
+    //listen for reuqests
+    app.listen(PORT, () => {
+      console.log('server is listening to port ' + PORT)
     })
-    .catch((err) => {
-        console.log(err);
-    });
+  })
+  .catch((err) => {
+    console.log(err)
+  })
 
-app.use(bodyParser.json());
-
+app.use(bodyParser.json())
 
 app.get('/', function (req, res) {
-    res.send('Welcome to SCast Backend');
-});
-app.use('/user', userRoutes);
-app.use('/module', moduleRoutes);
-app.use('/post', postRoutes);
-app.use('/comment', commentRoutes);
+  res.send('Welcome to SCast Backend')
+})
+app.use('/user', userRoutes)
+app.use('/module', moduleRoutes)
+app.use('/post', postRoutes)
+app.use('/comment', commentRoutes)
 
 app.use((req, res) => {
-    res.send('Page not found in SCast');
-});
+  res.send('Page not found in SCast')
+})
